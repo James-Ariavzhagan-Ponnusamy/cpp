e1b7e57e58a3560f74def00101364066203dd3bf
James Arivazhagan <James.Arivazhagan@gmail.com>
2021-01-02 22:15:30 +0530

    Update split.cpp

--- strings/split.cpp
+++ strings/split.cpp
@@ -29,37 +29,6 @@ int main(){
   }
 }
 
-#include <string>
-#include <iostream>
-#include <map>
-#include <sstream>
-
-typedef std::map<std::string, int> map_str_int;
-
-void jsplit(std::string &s, map_str_int &m){
-  std::string token;
-  std::istringstream str(s);
-  while(getline(str, token, ' ')){
-    std::cout << token <<std::endl;
-    if ( m.find(token) != m.end()){
-        m[token]++;
-    }
-    else{
-        m[token] = 1;
-    }
-  }
-}
-int main(){
-  map_str_int m;    
-  std::string s;
-  std::cout << "Enter a string : " ;
-  getline(std::cin, s);
-  jsplit(s, m);
-  for(auto x : m){
-    std::cout << x.first << " : " << x.second << std::endl;  
-  }
-}
-
 /*
     Enter a string : this is is the the hello world world                                                                                           
     this                                                                                                                                            
