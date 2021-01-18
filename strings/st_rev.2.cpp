ced5c80d9fedc15bfc2b56ffdb203e759708a154
James Arivazhagan <James.Arivazhagan@gmail.com>
2021-01-18 15:06:31 +0530

    Add files via upload

---
+++ strings/st_rev.2.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <iostream>
+#include <algorithm>
+
+void InplaceReplace(std::string &s)
+{
+  size_t pos1=0,pos2=0;
+  while((pos2 = s.find_first_of(" \t", pos1)) != std::string::npos)
+  {
+    std::reverse(s.begin()+pos1, s.begin()+pos2);
+    pos1 = pos2 + 1 ;
+  }
+  std::reverse(s.begin()+pos1, s.end());
+}
+//sihT si skeegrofskeeg
+
+int main()
+{
+  std::string s = "This	is	geeksforgeeks";
+  InplaceReplace(s);
+  std::cout << s << std::endl;
+} 
+
+//sihT    si      skeegrofskeeg
